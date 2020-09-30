2742f64dadef8fc7bfaa8fe5ea5736297e108887
Chetan Jain <JAIN.CJ.CHETAN@gmail.com>
2020-09-30 22:36:27 +0530

    setting up stuff

--- services/cache.js
+++ services/cache.js
@@ -1,20 +1,25 @@
 const mongoose = require("mongoose");
-
 const redis = require("redis");
 const redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
 const util = require("util");
 
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = () => {
+mongoose.Query.prototype.cache = function (options = {}) {
     this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || "");
+    console.log("picking from cache");
+    return this;
 };
 //TODO: "this" is a reference to query>.prototype
 
 mongoose.Query.prototype.exec = async function () {
-    if (!this.useCache) return await exec.apply(this, arguments);
+    if (!this.useCache) {
+        console.log("not picking from cache");
+        return exec.apply(this, arguments);
+    }
 
     console.log("running a query");
     //TODO:to safely copy objects
@@ -23,28 +28,28 @@ mongoose.Query.prototype.exec = async function () {
             collection: this.mongooseCollection.name,
         })
     );
+    console.log("we have key");
     // check if any cached data in redis related to key
-    const cachedValues = await client.get(key);
-    client.on("error", function (error) {
-        console.error(error);
-    });
+    const cachedValues = await client.get(this.hashKey, key);
+    console.log("got cached value", cachedValues);
     //if yes, return
-    if (cachedValues) {
-        const doc = JSON.parse(cachedValues);
-        return Array.isArray(doc)
-            ? doc.map((d) => new this.model(d))
-            : new this.model(doc);
-    }
-
+    // if (cachedValues) {
+    //     console.log("cache exists", cachedValues);
+    //     const doc = JSON.parse(cachedValues);
+    //     return Array.isArray(doc)
+    //         ? doc.map((d) => new this.model(d))
+    //         : new this.model(doc);
+    // }
     // if no, respond to request
     // and cache data in store
-
-    console.log("key :", key);
+    console.log("key :-----------------------------------", key);
     const result = await exec.apply(this, arguments);
-    client.set(key, JSON.stringify(result), EX, 10);
-    console.log("result : ", result);
+    client.hset(this.hashKey, key, JSON.stringify(result), "EX", 10);
     return result;
 };
 
-// client.set(req.user.id, JSON.stringify(blogs));
-// console.log("no redis here");
+module.exports = {
+    clearHash(hashKey) {
+        client.del(JSON.stringify(hashKey));
+    },
+};
