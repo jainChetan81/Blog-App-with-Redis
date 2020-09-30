8c63e8fe33909c44de1791946cd5d0b11da76625
Chetan Jain <JAIN.CJ.CHETAN@gmail.com>
2020-09-30 10:14:08 +0530

    blog downloaded and added a beta redis

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID:
+        "895459990820-epdtj7uinmbl891otrarucl3kmkmrbb9.apps.googleusercontent.com",
+    googleClientSecret: "902jka5CBl1RqfP1odmhXGHe",
+    mongoURI: "mongodb://chetan:abc123abc@ds123372.mlab.com:23372/cjemaily-dev",
+    cookieKey: "coolboy",
+};
